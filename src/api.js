3f5165e862b2698b5e999ecbfe14a9267da0cc0a
Scott Everson <148817937+scottie-eves@users.noreply.github.com>
2024-11-23 13:40:38 -0800

    api.js update

--- src/api.js
+++ src/api.js
@@ -1,88 +1,96 @@
-import mockData from "./mock-data";
+import mockData from './mock-data';
 
-
-// This function takes an events array, then uses map to create a new array with only locations.
-// It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
-// The Set will remove all duplicates from the array.
-
+/**
+ * This function takes an events array, then uses map to create a new array with only locations.
+ * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
+ * The Set will remove all duplicates from the array.
+ */
 export const extractLocations = (events) => {
-    const extractLocations = events.map((event) => event.location);
-    const locations = [...new Set(extractLocations)];
-    return locations;
+  const extractedLocations = events.map((event) => event.location);
+  const locations = [...new Set(extractedLocations)];
+  return locations;
+};
+
+/**
+ *
+ * This function will fetch the list of all events
+ */
+export const getEvents = async () => {
+
+  if (window.location.href.startsWith("http://localhost")) {
+    return mockData;
+  }
+
+  const token = await getAccessToken();
+
+  if (token) {
+    removeQuery();
+    const url = "https://pxdyls9agj.execute-api.us-west-2.amazonaws.com/dev/api/get-events" + "/" + token;
+    const response = await fetch(url);
+    const result = await response.json();
+    if (result) {
+      return result.events;
+    } else return null;
+  }
+};
+
+
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem('access_token');
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const response = await fetch(
+        "https://pxdyls9agj.execute-api.us-west-2.amazonaws.com/dev/api/get-auth-url"
+      );
+      const result = await response.json();
+      const { authUrl } = result;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
 };
 
 const checkToken = async (accessToken) => {
-    const response = await fetch(
-        `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-    );
-    const result = await response.json();
-    return result;
+  const response = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  );
+  const result = await response.json();
+  return result;
 };
 
 const removeQuery = () => {
-    let newurl;
-    if (window.history.pushState && window.location.pathname) {
-        newurl = 
-        window.location.protocol +
-        "//" +
-        window.location.host +
-        window.location.pathname;
-        window.history.pushState("", "", newurl);
-    } else {
-        newurl = window.location.protocol + "//" + window.location.host;
-        window.history.pushState("", "", newurl);
-    }
+  let newurl;
+  if (window.history.pushState && window.location.pathname) {
+    newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState("", "", newurl);
+  } else {
+    newurl = window.location.protocol + "//" + window.location.host;
+    window.history.pushState("", "", newurl);
+  }
 };
 
-// This function will fetch the list of all events.
-
-export const getEvents = async () => {
-    if (window.location.href.startsWith("http://localhost")) {
-      return mockData;
-    }
-  
-    const token = await getAccessToken();
-  
-    if (token) {
-      removeQuery();
-      const url =  "https://pxdyls9agj.execute-api.us-west-2.amazonaws.com/dev/api/get-events" + "/" + token;
-      const response = await fetch(url);
-      const result = await response.json();
-      if (result) {
-        return result.events;
-      } else return null; 
-    }
-  };
-
-  const getToken = async (code) => {
+const getToken = async (code) => {
+  try {
     const encodeCode = encodeURIComponent(code);
-    const response = await fetch(
-        'https://pxdyls9agj.execute-api.us-west-2.amazonaws.com/dev/api/token' + '/' + encodeCode
-    );
+
+    const response = await fetch('https://pxdyls9agj.execute-api.us-west-2.amazonaws.com/dev/api/token' + '/' + encodeCode);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
     const { access_token } = await response.json();
     access_token && localStorage.setItem("access_token", access_token);
-
     return access_token;
-  };
-
-export const getAccessToken = async () => {
-    const accessToken = localStorage.getItem('access_token');
-
-    const tokenCheck = accessToken && (await checkToken(accessToken));
-
-    if (!accessToken || tokenCheck.error) {
-        await localStorage.removeItem("access_token");
-        const searchParams = new URLSearchParams(window.location.search);
-        const code = await searchParams.get("code");
-        if (!code) {
-        const response = await fetch(
-            "https://pxdyls9agj.execute-api.us-west-2.amazonaws.com/dev/api/get-auth-url"
-        );
-        const result = await response.json();
-        const { authUrl } = result;
-        return (window.location.href = authUrl);
-        }
-        return code && getToken(code);
-    }
-  return accessToken;
+  } catch (error) {
+    error.json();
+  }
 };
